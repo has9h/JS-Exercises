d32cb1cdd423553fa112dd91953f04665d381c1c
Hasnain Hossain <has9h2@gmail.com>
2018-10-08 02:31:15 +0000

    Task 3 completed

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ function sumArray(arr){
 }
 
 console.log(sumArray([2, 4, 5, 6, 7]));
+console.log(sumArray([2, 43, 5, 6, 7]));
+
+//Task 3
+
+function checkEmail(emailString){
+    var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    var match = emailFormat.test(emailString);
+    return match;
+}
+
+console.log(checkEmail("has9h2@gmail.com"));
+console.log(checkEmail("has9h2gmail.com"));

ba726d161067d54f4dc50a27460829c7b9bc39b3
Hasnain Hossain <has9h2@gmail.com>
2018-10-08 02:54:57 +0000

    Task 4 completed

--- script.js
+++ script.js
@@ -26,3 +26,32 @@ function checkEmail(emailString){
 
 console.log(checkEmail("has9h2@gmail.com"));
 console.log(checkEmail("has9h2gmail.com"));
+
+//Task 4
+
+var library = [ 
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games', 
+       readingStatus: false
+   }];
+
+function getReadingStatus(index){
+    var book = library[index];
+    return book.readingStatus;
+    // return library[index].readingStatus;
+}
+
+console.log(getReadingStatus(1));
+console.log(getReadingStatus(0));
+console.log(getReadingStatus(2));
